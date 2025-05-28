e58b0954f2e0f2c590de13ee937dd35b776bd836
SBIHI-Elhabib <145591766+SBIHI-Elhabib@users.noreply.github.com>
2025-05-28 11:16:06 +0100

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://github.com/SBIHI-Elhabib/iphone-case-makeup', // Replace with your GitHub repo name
+  base: '/iphone-case-makeup/', // Replace with your GitHub repo name
   plugins: [react()],
 })
